a16e8b040234e9689669ee398c98d4563fd0bde9
Katana Tran <tkatyk21@gmail.com>
2019-06-24 18:06:27 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -122,7 +122,7 @@ def num_points_scored(player_name)
   array = []
   game_hash.each do |team, data|
     data.each do |name, stats|
-     if name == (player_name)
+     if name == player_name
        array << game_hash[team][data][name][:points]
     end
   end
